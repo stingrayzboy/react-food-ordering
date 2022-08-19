54798f25574f021dbd20e323ac791cccb13d0a4f
Faraz Noor <faraznoor75@gmail.com>
2022-08-19 22:09:43 +0530

    Built the InputForm

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,5 +1,6 @@
 import classes from "./MealItem.module.css";
 import React from "react";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const price = `$${props.price.toFixed(2)}`;
@@ -10,7 +11,7 @@ const MealItem = (props) => {
       </div>
       <div className={classes.description}>{props.description}</div>
       <div className={classes.price}>{price}</div>
-      <div></div>
+      <div><MealItemForm id={props.id}/></div>
     </li>
   );
 };
